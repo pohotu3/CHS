bb55a7cdf42768c5e2f3a8f25d01f85571e36dab
Austin VanAlstyne <ajv857@gmail.com>
2015-05-13 14:45:23 -0500

    Created

---
+++ HomeSystem.cpp
@@ -0,0 +1,19 @@
+/*
+* HomeSystem
+* Created by Austin and Ezra
+* Open Source with Related GitHub Repo
+* UNDER DEVELOPMENT
+*/
+
+#include "stdafx.h"
+#include "sysParams.h"
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	cout << systemName << " " << systemType << " Version " << version << "\nCreated By Ezra and Austin\n\n" << endl;
+	getchar();
+	return 0;
+}
---
+++ sysParams.h
@@ -0,0 +1,10 @@
+/*
+Global System Parameters
+*/
+
+char systemName[] = "HomeSystem";
+
+char systemType[] = "Shard";
+//char systemType[] = "Server";
+
+float version = 0.1;
