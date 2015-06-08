bc52e3157638b4c2073697fc0f18fefdd4ad8d45
Austin VanAlstyne <ajv857@gmail.com>
2015-06-08 13:59:22 -0500

    Creation of the speech

---
+++ Speech.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Speech;
+using System.Speech.Synthesis;
+
+namespace HomeSystem_CSharp
+{
+    class Speech
+    {
+        private SpeechSynthesizer synth = new SpeechSynthesizer();
+
+        public Speech()
+        {
+            synth.SetOutputToDefaultAudioDevice();
+            synth.Speak("This is a really neat test of the speech software");
+        }
+
+
+    }
+}
