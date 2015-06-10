4da5b6b871241a74490cb8549a8862122fcb33e4
Austin VanAlstyne <ajv857@gmail.com>
2015-06-09 21:55:12 -0500

    added license info to speech

--- Speech.cs
+++ Speech.cs
@@ -1,4 +1,30 @@
-﻿using System;
+﻿/*
+* Crystal Home Systems 
+* Created by Austin and Ezra
+* Open Source with Related GitHub Repo
+* UNDER DEVELOPMENT
+*
+* Copyright© 2015 Austin VanAlstyne, Bailey Thorson
+*/
+
+/*
+*This file is part of Cyrstal Home Systems.
+*
+*Cyrstal Home Systems is free software: you can redistribute it and/or modify
+*it under the terms of the GNU General Public License as published by
+*the Free Software Foundation, either version 3 of the License, or
+*(at your option) any later version.
+*
+*Cyrstal Home Systems is distributed in the hope that it will be useful,
+*but WITHOUT ANY WARRANTY; without even the implied warranty of
+*MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*GNU General Public License for more details.
+*
+*You should have received a copy of the GNU General Public License
+*along with Cyrstal Home Systems.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
