8625050fbddc1505f3031c34464c1fc6f1348969
Austin VanAlstyne <ajv857@gmail.com>
2015-06-24 21:47:10 -0500

    added Google.Apis.Drive.v2

---
+++ Server-Version/Patch.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrystalHomeSystems
+{
+    class Patch
+    {
+        public Patch(bool googleDriveTest)
+        {
+            if (googleDriveTest)
+                googleDrivePatch();
+            else
+            {
+
+            }
+
+        }
+
+        private void googleDrivePatch()
+        {
+
+        }
+    }
+}
