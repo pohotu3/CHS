b644269aac485155a9bf1ddffd0aafc6a034d4b5
Austin VanAlstyne <ajv857@gmail.com>
2015-07-05 15:13:40 -0500

    set up the comment stream "framework"

--- Heart/Heart/Core.cs
+++ Heart/Heart/Core.cs
@@ -5,10 +5,19 @@ namespace Heart
 	class Core
 	{
 
+		private string systemType = "Heart", version = "0.0.1", systemName = "Crystal Home Systems";
 
 		public static void Main (string[] args)
 		{
-			
+			// initialize the configuration files first
+
+			// if first time setup hasn't been done, do that here
+
+			// because this is the Heart, we do not need to initialize the speech
+
+			// initialize the console application next (we can make this a GUI if we want)
+			// another thing we could do is set up the console to be a browser based setup, using php or
+			// something, allowing remote connection instead of having to directly link up
 		}
 	}
 }
