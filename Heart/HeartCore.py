29bb782d9dbdf7017fd807b437737e052c10fcb5
Austin VanAlstyne <ajv857@gmail.com>
2015-08-02 22:58:19 -0500

    Created logging for the server

--- Heart/HeartCore.py
+++ Heart/HeartCore.py
@@ -1,1 +1,22 @@
- 
+from datetime import datetime
+from os.path import expanduser
+
+class Log:
+    now = datetime.now()
+    fileName = str(now.month) + "-" + str(now.day) + "-" + str(now.year)
+    home = expanduser("~")
+    fileDir = home + "/CrystalHomeSys/Heart/Logs/" + fileName + ".txt"
+    logFile = open(fileDir, "a")
+    
+    def __init__(self):
+        self.write("-------------SYSTEM STARTUP----------------")
+        self.write("Welcome to Crystal Home Systems")
+        self.write("Created Log file at " + self.fileDir)
+        return
+    
+    def write(self, s):
+        print(s)
+        self.logFile.write(str(self.now.hour) + ":" + str(self.now.minute) + ":" + str(self.now.second) + ">>> " + s + "\n")
+        return
+
+log = Log() # create my log object
