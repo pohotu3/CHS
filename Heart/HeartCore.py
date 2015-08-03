e9a26e32859a6976fade359074e47045a66ab7f7
Austin VanAlstyne <ajv857@gmail.com>
2015-08-03 11:24:54 -0500

    Added an HTTP server and dynamically searches for an available port

--- Heart/HeartCore.py
+++ Heart/HeartCore.py
@@ -1,5 +1,7 @@
 from datetime import datetime
 from os.path import expanduser
+from http.server import BaseHTTPRequestHandler, HTTPServer
+import os
 
 class Log:
     now = datetime.now()
@@ -18,5 +20,45 @@ class Log:
         print(s)
         self.logFile.write(str(self.now.hour) + ":" + str(self.now.minute) + ":" + str(self.now.second) + ">>> " + s + "\n")
         return
+    
+class HTTPRequestHandler(BaseHTTPRequestHandler):
+    #handle GET command
+    def do_GET(self):
+        rootdir = expanduser("~") + "/CrystalHomeSys/" #file location
+        try:
+            if self.path.endswith('.html'):
+                f = open(rootdir + self.path) #open requested file
+                
+                #send code 200 response
+                self.send_response(200)
+            
+                #send header first
+                self.send_header('Content-type','text-html')
+                self.end_headers()
+                
+                #send file content to client
+                self.wfile.write(f.read())
+                f.close()
+                return
+      
+        except IOError:
+            self.send_error(404, 'file not found')
 
 log = Log() # create my log object
+
+#ip and port of server
+#by default http server port is 80
+ip = '127.0.0.1'
+port = 8081
+port_works = False
+while port_works == False:
+    try:
+        server_address = (ip, port)
+        httpd = HTTPServer(server_address, HTTPRequestHandler)
+        port_works = True
+    except:
+        port = port + 1
+
+log.write("Starting HTTP server on ip " + ip + " port " + str(port))
+log.write('HTTP server is running... Press ^Z to quit.')
+httpd.serve_forever()
