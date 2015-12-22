1da2328cde42a2a4ad02008de1201ad0162fc5cc
Austin VanAlstyne <ajv857@gmail.com>
2015-12-21 21:24:17 -0600

    added the Nerv project, working in GTK. Also added the client file to Nerv, now have to modify everything to make it work

---
+++ Nerv/Client.cs
@@ -0,0 +1,194 @@
+﻿/*
+* Crystal Home Systems 
+* Created by Austin and Ezra
+* Open Source with Related GitHub Repo
+* UNDER DEVELOPMENT
+*
+* Copyright© 2015 Austin VanAlstyne, Bailey Thorson
+*/
+
+/*
+*This file is part of Cyrstal Home Systems.
+*
+*Cyrstal Home Systems is free software: you can redistribute it and/or modify
+*it under the terms of the GNU General Public License as published by
+*the Free Software Foundation, either version 3 of the License, or
+*(at your option) any later version.
+*
+*Cyrstal Home Systems is distributed in the hope that it will be useful,
+*but WITHOUT ANY WARRANTY; without even the implied warranty of
+*MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*GNU General Public License for more details.
+*
+*You should have received a copy of the GNU General Public License
+*along with Cyrstal Home Systems.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+using System;
+using System.Net;
+using System.Net.Sockets;
+using System.IO;
+//using System.Windows.Threading;
+using System.Threading;
+using ConnectionData;
+
+namespace Nerv
+{
+    public class Client
+    {
+
+        private int port;
+        private string ipAddress, guid, serverGuid;
+        private bool running = false;
+        public bool connected = false; // unable to use Data_OUT() if this is set to false
+
+        public Socket socket;
+        private Thread listeningThread;
+
+        // requires an IP and socket constructor
+        public Client(string ipAddress, int port, Guid guid)
+        {
+            this.port = port;
+            this.ipAddress = ipAddress;
+            this.guid = guid.ToString();
+
+            socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            IPEndPoint ip = new IPEndPoint(IPAddress.Parse(ipAddress), port);
+
+            try
+            {
+                // tries to connect to the socket located at the IP and socket given
+                socket.Connect(ip);
+                Core.GetCore().Log("Connected to Heart at IP " + ipAddress + " Socket: " + port);
+                Core.GetCore().Speak("Connected.");
+            }
+            catch
+            {
+                Core.GetCore().Log("Could not connect to " + ipAddress + " on Socket: " + port);
+                Core.GetCore().Speak("I was unable to connect to my Heart. Please reboot me and try again.");
+                Thread.Sleep(1000);
+
+                return; // we dont want the client socket continuing if we were not able to connect
+            }
+
+            // creates the new listening thread
+            listeningThread = new Thread(Data_IN);
+            running = true;
+            listeningThread.Start(); // Data_IN is now constantly being run
+        }
+
+        private void Data_IN()
+        {
+            byte[] buffer;
+            int readBytes;
+
+            while (running)
+            {
+                buffer = new byte[socket.SendBufferSize]; // creates the buffer array to be as large as possible to receive
+
+                try
+                {
+                    readBytes = socket.Receive(buffer); // gets the number of bytes received
+                }
+                catch (Exception e)
+                {
+                    return;
+                }
+
+                // as long as we actually received bytes, we can process them
+                if (readBytes > 0)
+                    DataManager(new Packet(buffer));
+            }
+        }
+
+        // Unable to send data if bool connected = false
+        public void Data_OUT(Packet p)
+        {
+            if (connected)
+            {
+                try
+                {
+                    socket.Send(p.ToBytes());
+                }
+                catch (Exception e)
+                {
+                }
+            }
+            else
+            {
+                if (p.packetType != Packet.PacketType.CloseConnection)
+                {
+                    Core.GetCore().Log("Not connected to Heart. Cannot send packet. Packet type: " + p.packetType.ToString() + " Packet String: " + p.packetString);
+                    Core.GetCore().Speak("I am not connected to my Heart, so I cant send data. Please restart me to attempt to fix the problem.");
+                    return;
+                }
+            }
+        }
+
+        // for handling received data
+        private void DataManager(Packet p)
+        {
+            if (p.senderID != serverGuid && connected) // if the packet ID isn't from the server
+            {
+                Core.GetCore().Log("Unauthorized Packet SenderID found. Closing connection.");
+                Core.GetCore().Write("An unauthorized connection is attempting to control me. Shutting down.");
+                Core.GetCore().Shutdown();
+            }
+
+            switch (p.packetType)
+            {
+                case Packet.PacketType.Registration: // if the server is sending the registration packet (start of connection)
+                    Core.GetCore().Log("Received registration packet from Heart.");
+                    serverGuid = p.senderID;
+                    Core.commandKey = p.packetString; // set the commandKey from the server registration packet
+
+                    Packet temp = new Packet(Packet.PacketType.Registration, guid); // client registration packet
+                    Core.GetCore().Log("Sending Registration");
+                    socket.Send(temp.ToBytes());    // send packet to heart
+
+                    Core.GetCore().Log("Sent registration packet to Heart.");
+                    break;
+                case Packet.PacketType.Handshake:
+                    connected = true;
+                    Core.GetCore().Log("Handshake received. Connection Established.");
+                    Core.GetCore().Speak("I've connected to my Heart!");
+                    break;
+                case Packet.PacketType.CloseConnection:
+                    string reason = p.packetString;
+                    if (reason == null)
+                        reason = "It didn't specify a reason, so please reboot both my Heart and I.";
+                    Core.GetCore().Log("Server is closing the connection. Reason: " + reason);
+                    Core.GetCore().Speak("My Heart decided to close the connection. " + reason + ".");
+                    Close();
+                    break;
+                case Packet.PacketType.Command:
+                    Core.GetCore().Log("Server sent the command: " + p.packetString);
+                    HandleCommand(p.packetString);
+                    break;
+                case Packet.PacketType.Error:
+                    Core.GetCore().Log("Error message from Heart. Message: " + p.packetString + ". Shutting down.");
+                    Core.GetCore().Speak("We recieved an error from the Heart. Shutting down. Please view logs for further details.");
+                    Core.GetCore().Shutdown();
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        private void HandleCommand(string c)
+        {
+
+        }
+
+        public void Close()
+        {
+            Data_OUT(new Packet(Packet.PacketType.CloseConnection, guid));
+            Core.GetCore().Write("Closing connection with Heart.");
+            running = false;
+            socket.Close();
+            socket.Dispose();
+            listeningThread.Abort();
+        }
+    }
+}
+
---
+++ Nerv/Core.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Gtk;
+
+namespace Nerv
+{
+	class Core
+	{
+
+		private static Core core;
+		private static MainWindow mw;
+
+		public static string commandKey = "";
+
+		public Core(){
+			core = this;
+		}
+
+		public static Core GetCore(){
+			return core;
+		}
+
+		public void Log(string s){
+
+		}
+
+		public void Speak(string s){
+
+		}
+
+		public void Write(string s){
+		}
+
+		public void Shutdown(){
+		}
+
+		public static void Main (string[] args)
+		{
+			new Core ();
+			Application.Init ();
+			mw = new MainWindow ();
+			mw.Show ();
+			Application.Run ();
+		}
+	}
+}
