37f4aebb0b08d7653106a537043c3d84f5365373
Austin VanAlstyne <ajv857@gmail.com>
2016-01-20 13:37:10 -0600

    Set up the initial ShardCore.cs file in /Shard

---
+++ Shard/Client.cs
@@ -0,0 +1,193 @@
+﻿/*
+* Crystal Home Systems 
+* Created by Austin and Ezra
+* Open Source with Related GitHub Repo
+* UNDER DEVELOPMENT
+*
+* Copyright© 2015 Austin VanAlstyne, Bailey Thorson
+*/
+
+/*
+*This file is part of Cyrstal Home Systems.
+*
+*Cyrstal Home Systems is free software: you can redistribute it and/or modify
+*it under the terms of the GNU General Public License as published by
+*the Free Software Foundation, either version 3 of the License, or
+*(at your option) any later version.
+*
+*Cyrstal Home Systems is distributed in the hope that it will be useful,
+*but WITHOUT ANY WARRANTY; without even the implied warranty of
+*MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*GNU General Public License for more details.
+*
+*You should have received a copy of the GNU General Public License
+*along with Cyrstal Home Systems.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+using System;
+using System.Net;
+using System.Net.Sockets;
+using System.IO;
+using System.Threading;
+using ConnectionData;
+
+namespace Shard
+{
+    public class Client
+    {
+
+        private int port;
+        private string ipAddress, guid, serverGuid;
+        private bool running = false;
+        public bool connected = false; // unable to use Data_OUT() if this is set to false
+
+        public Socket socket;
+        private Thread listeningThread;
+
+        // requires an IP and socket constructor
+        public Client(string ipAddress, int port, Guid guid)
+        {
+            this.port = port;
+            this.ipAddress = ipAddress;
+            this.guid = guid.ToString();
+
+            socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            IPEndPoint ip = new IPEndPoint(IPAddress.Parse(ipAddress), port);
+
+            try
+            {
+                // tries to connect to the socket located at the IP and socket given
+                socket.Connect(ip);
+                ShardCore.GetCore().Log("Connected to Heart at IP " + ipAddress + " Socket: " + port);
+                ShardCore.GetCore().Speak("Connected.");
+            }
+            catch
+            {
+                ShardCore.GetCore().Log("Could not connect to " + ipAddress + " on Socket: " + port);
+                ShardCore.GetCore().Speak("I was unable to connect to my Heart. Please reboot me and try again.");
+                Thread.Sleep(1000);
+
+                return; // we dont want the client socket continuing if we were not able to connect
+            }
+
+            // creates the new listening thread
+            listeningThread = new Thread(Data_IN);
+            running = true;
+            listeningThread.Start(); // Data_IN is now constantly being run
+        }
+
+        private void Data_IN()
+        {
+            byte[] buffer;
+            int readBytes;
+
+            while (running)
+            {
+                buffer = new byte[socket.SendBufferSize]; // creates the buffer array to be as large as possible to receive
+
+                try
+                {
+                    readBytes = socket.Receive(buffer); // gets the number of bytes received
+                }
+                catch (Exception e)
+                {
+                    return;
+                }
+
+                // as long as we actually received bytes, we can process them
+                if (readBytes > 0)
+                    DataManager(new Packet(buffer));
+            }
+        }
+
+        // Unable to send data if bool connected = false
+        public void Data_OUT(Packet p)
+        {
+            if (connected)
+            {
+                try
+                {
+                    socket.Send(p.ToBytes());
+                }
+                catch (Exception e)
+                {
+                }
+            }
+            else
+            {
+                if (p.packetType != Packet.PacketType.CloseConnection)
+                {
+                    ShardCore.GetCore().Log("Not connected to Heart. Cannot send packet. Packet type: " + p.packetType.ToString() + " Packet String: " + p.packetString);
+                    ShardCore.GetCore().Speak("I am not connected to my Heart, so I cant send data. Please restart me to attempt to fix the problem.");
+                    return;
+                }
+            }
+        }
+
+        // for handling received data
+        private void DataManager(Packet p)
+        {
+            if (p.senderID != serverGuid && connected) // if the packet ID isn't from the server
+            {
+                ShardCore.GetCore().Log("Unauthorized Packet SenderID found. Closing connection.");
+                ShardCore.GetCore().Write("An unauthorized connection is attempting to control me. Shutting down.");
+                ShardCore.GetCore().Shutdown();
+            }
+
+            switch (p.packetType)
+            {
+                case Packet.PacketType.Registration: // if the server is sending the registration packet (start of connection)
+                    ShardCore.GetCore().Log("Received registration packet from Heart.");
+                    serverGuid = p.senderID;
+                    ShardCore.commandKey = p.packetString; // set the commandKey from the server registration packet
+
+                    Packet temp = new Packet(Packet.PacketType.Registration, guid); // client registration packet
+                    ShardCore.GetCore().Log("Sending Registration");
+                    socket.Send(temp.ToBytes());    // send packet to heart
+
+                    ShardCore.GetCore().Log("Sent registration packet to Heart.");
+                    break;
+                case Packet.PacketType.Handshake:
+                    connected = true;
+                    ShardCore.GetCore().Log("Handshake received. Connection Established.");
+                    ShardCore.GetCore().Speak("I've connected to my Heart!");
+                    break;
+                case Packet.PacketType.CloseConnection:
+                    string reason = p.packetString;
+                    if (reason == null)
+                        reason = "It didn't specify a reason, so please reboot both my Heart and I.";
+                    ShardCore.GetCore().Log("Server is closing the connection. Reason: " + reason);
+                    ShardCore.GetCore().Speak("My Heart decided to close the connection. " + reason + ".");
+                    Close();
+                    break;
+                case Packet.PacketType.Command:
+                    ShardCore.GetCore().Log("Server sent the command: " + p.packetString);
+                    HandleCommand(p.packetString);
+                    break;
+                case Packet.PacketType.Error:
+                    ShardCore.GetCore().Log("Error message from Heart. Message: " + p.packetString + ". Shutting down.");
+                    ShardCore.GetCore().Speak("We recieved an error from the Heart. Shutting down. Please view logs for further details.");
+                    ShardCore.GetCore().Shutdown();
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        private void HandleCommand(string c)
+        {
+
+        }
+
+        public void Close()
+        {
+            Data_OUT(new Packet(Packet.PacketType.CloseConnection, guid));
+            ShardCore.GetCore().Write("Closing connection with Heart.");
+            running = false;
+            socket.Close();
+            socket.Dispose();
+            listeningThread.Abort();
+        }
+    }
+}
+
---
+++ Shard/ShardCore.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using ConnectionData;
+
+namespace Shard
+{
+    class ShardCore
+    {
+        private Client client;
+        private static ShardCore core;
+        private Log log;
+        private Config cfg;
+        private Guid guid;
+
+        public static string commandKey, systemName = "Crystal Shard Dev", baseDir = "/CrystalHomeSys/", logBaseDir = "Logs/", configDir = "shard_config.cfg";
+
+        public ShardCore()
+        {
+            baseDir = System.Environment.GetEnvironmentVariable("HOME") + baseDir;
+            logBaseDir = baseDir + logBaseDir;
+            configDir = baseDir + configDir;
+
+            log = new Log(logBaseDir);
+
+            core = this;
+            Log("####################" + systemName + " has been Started#####################");
+
+            cfg = new Config(configDir);
+            LoadConfig();
+
+            Write("Setting up client...");
+            Log("Setting up client...");
+            client = new Client("127.0.0.1", 6976, guid);
+        }
+
+        public void Write(string s)
+        {
+
+        }
+
+        public void Log(string s)
+        {
+            Write(s);
+            log.write(s);
+            try
+            {
+                Packet logPacket = new Packet(Packet.PacketType.ShardLog, guid.ToString());
+                logPacket.packetString = s;
+                SendPacket(logPacket);
+            }
+            catch (Exception e) { }
+        }
+
+        private void LoadConfig()
+        {
+            try
+            {
+                guid = Guid.Parse(cfg.get("guid"));
+                Log("Configuration file found. Loading settings.");
+            }
+            catch (Exception e)
+            {
+                CreateCFG();
+            }
+        }
+
+        private void CreateCFG()
+        {
+            Log("Configuration file does not exist. Creating file.");
+            try
+            {
+                File.Delete(cfg.FileName());
+            }
+            catch (Exception e) { }
+
+            cfg.reload();
+            cfg.set("guid", Guid.NewGuid().ToString());
+            cfg.Save();
+
+            LoadConfig();
+        }
+
+        public Config GetConfig()
+        {
+            return cfg;
+        }
+
+        public void SendPacket(Packet p)
+        {
+            if (client.connected)
+                client.Data_OUT(p);
+        }
+
+        public void Speak(string s)
+        {
+
+        }
+
+        public static ShardCore GetCore()
+        {
+            return core;
+        }
+
+        public Client GetClient()
+        {
+            return client;
+        }
+
+        public void Shutdown()
+        {
+            client.Close();
+        }
+
+        static void Main(string[] args)
+        {
+            new ShardCore();
+        }
+    }
+}
