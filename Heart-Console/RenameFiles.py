1655d93ab61486671a53726b51974137d4ff0353
Austin VanAlstyne <ajv857@gmail.com>
2016-01-29 00:20:03 -0600

    Restart from scratch, remove (almost) every file

--- Heart-Console/RenameFiles.py
+++
@@ -1,5 +0,0 @@
-import os, sys
-
-#[os.rename(f, f.replace(' ', '_')) for f in os.listdir(str(sys.argv[1])) if not f.startswith('.')]
-for file in os.listdir(sys.argv[1]):
-	os.renames(file, file.replace(' ', '_'))
